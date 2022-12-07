2168ce2a33795edd21fa9a550a864d674f46a8ed
belalkhaleel <belalkhaleel19@gmail.com>
2022-12-07 11:18:19 +0200

    Step 6

---
+++ javascript-basics-01/Step_6.html + Step_6.js
@@ -0,0 +1,4 @@
+const num1 = parseInt(prompt('Enter the first number '));
+const num2 = parseInt(prompt('Enter the second number '));
+const remainder = num1 % num2;
+alert(`The remainder of ${num1} and ${num2} is ${remainder}`);
