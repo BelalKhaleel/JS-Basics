c64a953b37c6f010e6578dee085750e8881894b7
belalkhaleel <belalkhaleel19@gmail.com>
2022-12-07 15:55:12 +0200

    fixed step 5

--- javascript-basics-01/Step_4.html + Step_4.js
+++ javascript-basics-01/Step_4.html + Step_4.js
@@ -1,7 +1,6 @@
 let Name = prompt("What is your name?");
 alert("Hello, " + Name);
 let Surname = prompt("What is your surname?");
-
 let City = prompt("What is your city?");
 document.getElementById("name").value = Name;
 document.getElementById("surname").value = Surname;
--- javascript-basics-01/Step_5.html + Step_5.js
+++ javascript-basics-01/Step_5.html + Step_5.js
@@ -1,4 +1,8 @@
 const num1 = parseInt(prompt('Enter the first number '));
+document.getElementById("first_number").value = num1;
 const num2 = parseInt(prompt('Enter the second number '));
+document.getElementById("second_number").value = num2;
 const product = num1 * num2;
+document.getElementById("validate").onclick = function() {
 alert(`The product of ${num1} and ${num2} is ${product}`);
+}
