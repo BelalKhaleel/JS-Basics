7a377115e5c9cedf34267b4d3c70210f19908087
belalkhaleel <belalkhaleel19@gmail.com>
2022-12-07 16:09:07 +0200

    fixed step 7

--- javascript-basics-01/Step_7.html + Step_7.js
+++ javascript-basics-01/Step_7.html + Step_7.js
@@ -1,10 +1,15 @@
-const num1 = parseInt(prompt('Enter your shoe size '));
-const num2 = parseInt(prompt('Enter your birth year '));
-function myFunction(num, num2) {
-    let x = [(num1 * 2 + 5) * 50 - num2 + 1766];
+const num1 = prompt('Enter your shoe size ');
+document.getElementById("shoe_size").value = num1;
 
-    return x;
+const num2 = prompt('Enter your birth year ');
+document.getElementById("year").value = num2;
+
+var result = [(num1 * 2 + 5) * 50 - num2 + 1766]
+
+function myFunction() {
+    return result;
   }
-
+  document.getElementById("validate").onclick = function() {
   let y = myFunction(num1,num2);
   alert(y);
+  }
