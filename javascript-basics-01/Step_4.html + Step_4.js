1d895d3904b14a8859a57ff80395100fffaf8131
belalkhaleel <belalkhaleel19@gmail.com>
2022-12-07 10:45:19 +0200

    Step 4

---
+++ javascript-basics-01/Step_4.html + Step_4.js
@@ -0,0 +1,9 @@
+let Name = prompt("What is your name?");
+alert("Hello, " + Name);
+let Surname = prompt("What is your surname?");
+let City = prompt("What is your city?");
+alert(`
+Name : ${Name}
+Surname : ${Surname}
+City : ${City}
+`)
