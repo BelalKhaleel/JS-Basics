5315247acfa3b7e93a1134aede3954dfc6690ce4
belalkhaleel <belalkhaleel19@gmail.com>
2022-12-07 16:14:22 +0200

    fixed step 8

--- javascript-basics-01/Step_8.html + Step_8.js
+++ javascript-basics-01/Step_8.html + Step_8.js
@@ -1,8 +1,12 @@
 let age = prompt("What is your age?");
+document.getElementById("age").value = age;
+
 let answer;
 if (age >= 18) {
     answer = "you are over 18";
 } else {
    answer = "you are under 18";
 }
+document.getElementById("validate").onclick = function() {
 alert(answer);
+}
