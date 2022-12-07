08c191b6e73b6be306dd3e4f941ca2a092048505
belalkhaleel <belalkhaleel19@gmail.com>
2022-12-07 15:48:00 +0200

    fixed Step 4

--- javascript-basics-01/Step_4.html + Step_4.js
+++ javascript-basics-01/Step_4.html + Step_4.js
@@ -1,9 +1,15 @@
 let Name = prompt("What is your name?");
 alert("Hello, " + Name);
 let Surname = prompt("What is your surname?");
+
 let City = prompt("What is your city?");
+document.getElementById("name").value = Name;
+document.getElementById("surname").value = Surname;
+document.getElementById("city").value = City;
+document.getElementById("validate").onclick = function() {
 alert(`
 Name : ${Name}
 Surname : ${Surname}
 City : ${City}
 `)
+}
