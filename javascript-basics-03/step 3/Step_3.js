c46fef7bf08efecc890d42881c55115a04332de3
belalkhaleel <belalkhaleel19@gmail.com>
2022-12-09 13:39:56 +0200

    Step 3

---
+++ javascript-basics-03/step 3/Step_3.js
@@ -0,0 +1,9 @@
+document.querySelector(".color.green").onclick = function(){
+    document.getElementById("text").style.color = "green";
+}
+document.querySelector(".color.red").onclick = function(){
+    document.getElementById("text").style.color = "red";
+}
+document.querySelector(".color.blue").onclick = function(){
+    document.getElementById("text").style.color = "blue";
+}
