60cf5297ef8e365a863cdf2a34996d6b800eb3e5
belalkhaleel <belalkhaleel19@gmail.com>
2022-12-09 13:26:37 +0200

    Step 2

---
+++ javascript-basics-03/step 2/Step_2.js
@@ -0,0 +1,7 @@
+document.getElementById("show").onclick = function(){
+    document.getElementById("texte").style.display = "block";
+}
+
+document.getElementById("hide").onclick = function(){
+    document.getElementById("texte").style.display = "none";
+}
